ecbdc207a01f9ebf247a747fa4a490d6d61c0e7b
GalacticLemonade <aidenmoore1820@gmail.com>
2024-03-31 20:40:14 -0600

    add string validation

--- Assets/MultiplayerPopup.cs
+++ Assets/MultiplayerPopup.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Text.RegularExpressions;
+using TMPro;
 using UnityEngine;
 
 public class MultiplayerPopup : TextDialog
@@ -27,6 +28,13 @@ public class MultiplayerPopup : TextDialog
 			this.m_supportId.text = ((HatchManager.CurrentPlayer == null) ? string.Empty : HatchManager.CurrentPlayer.HatchCustomerID);
 		}
 	}
+	
+	static bool isAlphanumeric(string input)
+	{
+		string pattern = @"^[a-zA-Z0-9_]+$";
+		
+		return Regex.IsMatch(input, pattern);
+	}
 
 	public new void Open()
 	{
@@ -44,8 +52,31 @@ public class MultiplayerPopup : TextDialog
 	}
 	
 	public void Connect()
-	{
-		Debug.Log("hi");
+	{ 
+		string gameIDText = m_gameidField.GetComponent<TMP_InputField>().text;
+		string usernameText = m_usernameField.GetComponent<TMP_InputField>().text;
+
+		TextMeshProUGUI statusText = m_statusText.GetComponent<TextMeshProUGUI>();
+		
+		if (!isAlphanumeric(gameIDText))
+		{
+			statusText.text = "Invalid Game ID!";
+			Debug.Log("GameID input invalid");
+			return;
+		}
+		
+		statusText.text = "";
+		
+		if (!isAlphanumeric(usernameText))
+		{
+			statusText.text = "Invalid username!";
+			Debug.Log("Username input invalid");
+			return;
+		}
+		
+		statusText.text = "";
+		
+		Debug.Log($"connect with username of {usernameText} and gameid {gameIDText}");
 	}
 
 	private void OnDestroy()
@@ -61,6 +92,15 @@ public class MultiplayerPopup : TextDialog
 	
 	[SerializeField]
 	private GameObject m_canvas;
+	
+	[SerializeField]
+	private GameObject m_usernameField;
+
+	[SerializeField]
+	private GameObject m_gameidField;
+	
+	[SerializeField]
+	private GameObject m_statusText;
 
 	private static bool s_optionsBubbleShowed;
 }
